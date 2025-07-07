71814454d8d9ac11535fd0cbb28c91c0db2c5af6
naibhivenkat <venkat.naibhi@gmail.com>
2025-07-07 23:16:43 +0530

    Creatd new files -  Gradle files

--- app/src/main/java/com/shopersapp/MainActivity.kt
+++ app/src/main/java/com/shopersapp/MainActivity.kt
@@ -1,13 +1,11 @@
-package com.shopersapp
+package com.venkat.shopersapp
 
-import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        startActivity(Intent(this, LoginActivity::class.java))
-        finish()
+        setContentView(R.layout.activity_main)
     }
 }
