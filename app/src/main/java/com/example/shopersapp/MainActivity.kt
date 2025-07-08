380e6cd3bd58f2867e17f955ebf715fbcb64e372
naibhivenkat <venkat.naibhi@gmail.com>
2025-07-08 18:13:11 +0530

    .kt files update

--- app/src/main/java/com/example/shopersapp/MainActivity.kt
+++ app/src/main/java/com/example/shopersapp/MainActivity.kt
@@ -1,11 +1,15 @@
-package com.example.shopersapp
-
-import android.os.Bundle
-import androidx.appcompat.app.AppCompatActivity
+import android.os.Handler
+import android.os.Looper
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, 2000) // Wait for 2 seconds
     }
 }
